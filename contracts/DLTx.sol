ebe8f931fe3d867a18ba0ff74b47338dab36e8fa
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-07-15 12:41:35 +1000

    Feat:  Add metadata

--- contracts/DLTx.sol
+++ contracts/DLTx.sol
@@ -1,16 +1,38 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.6;
 
-import "hardhat/console.sol";
 import "./ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
+struct MetaData {
+  uint256 startdate;
+  uint256 enddate;
+}
 
 contract DLTx is ERC721, Ownable {
 
   uint256 public totalSupply;
+  mapping (uint256 => MetaData) public mesh;
 
   function mint(address to) public onlyOwner() {
-    _mint(to, totalSupply);
+    mint(to, block.timestamp);
+  }
+
+  function mint(address to, uint256 startDate) public onlyOwner() {
+    require(to != address(0), "Invalid address");
+
+    mesh[totalSupply] = MetaData(startDate, 0);
+    _safeMint(to, totalSupply);
     totalSupply += 1;
+  }
+
+  function terminate(uint256 index) public onlyOwner() {
+    terminate(index, block.timestamp);
+  }
+
+  function terminate(uint256 index, uint256 enddate) public onlyOwner() {
+    require(index < totalSupply, "Invalid index");
+    require(mesh[index].enddate != 0, "Already terminated");
+    mesh[index].enddate = enddate;
   }
 }
