989ea7f10b205759ddd5c9ccc8ef20242a8354f7
Lorenzo Fontoura <lorenzo.fontoura@dltx.io>
2021-10-13 15:02:32 +1000

    allow setting start date; fix bugs

--- contracts/DLTx.sol
+++ contracts/DLTx.sol
@@ -14,33 +14,39 @@ contract DLTx is ERC721, Ownable {
   uint256 public totalSupply;
   mapping (uint256 => MetaData) public mesh;
 
-  function mint(address to) external onlyOwner() {
-    mint(to, block.timestamp, false);
-  }
-
   function mint(
       address to,
       uint256 startDate,
       bool probation
   ) public onlyOwner() {
     require(to != address(0), "Invalid address");
+    if (startDate == 0) startDate = block.timestamp;
     mesh[totalSupply] = MetaData(startDate, 0, true);
     _safeMint(to, totalSupply);
     totalSupply++;
   }
 
-  function terminate(uint256 index) external onlyOwner() {
+  function terminateNow(uint256 index) external onlyOwner() {
     terminate(index, block.timestamp);
   }
 
   function terminate(uint256 index, uint256 enddate) public onlyOwner() {
     require(index < totalSupply, "Invalid index");
-    require(mesh[index].enddate != 0, "Already terminated");
+    require(mesh[index].enddate == 0, "Already terminated");
     mesh[index].enddate = enddate;
   }
 
   function setProbation(uint256 index, bool value) external onlyOwner() {
     mesh[index].probation = value;
+  }
+
+  function setStartDate(uint256 index, uint256 value) external onlyOwner {
+    require(
+        mesh[index].enddate == 0 ||
+            mesh[index].startdate < mesh[index].enddate,
+        "Start date not lower than end"
+    );
+    mesh[index].startdate = value;
   }
 
   function employed(uint256 index) external view returns (bool) {
