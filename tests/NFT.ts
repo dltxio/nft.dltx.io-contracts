fd4a95699320b64fed598b7a4d7004536eacd49c
Lorenzo Fontoura <lorenzo.fontoura@dltx.io>
2022-07-22 15:52:14 +1000

    add transferable unit test

--- tests/NFT.ts
+++ tests/NFT.ts
@@ -1,17 +1,23 @@
 ﻿import { ethers } from "hardhat";
-import { ethers as tsEthers } from "ethers";
 import {deployProxy} from "../scripts/deploy/utils";
 import {expect} from "chai";
 import {Mesh} from "../build/typechain";
 import {constructorArguments} from "../scripts/deploy/contracts/Mesh";
+import {Signer, Wallet} from "ethers";
 
 let nft: Mesh;
-let deployer: tsEthers.Signer;
+let deployer: Signer;
+let meshie: Wallet;
 
 describe("NFT", () => {
   before(async () => {
     deployer = (await ethers.getSigners())[0];
-
+    meshie = Wallet.createRandom().connect(deployer.provider);
+    // fund meshie
+    await deployer.sendTransaction({
+      to: meshie.address,
+      value: ethers.utils.parseEther("10000000"),
+    });
     nft = (await deployProxy(
       "Mesh",
       constructorArguments(),
@@ -20,19 +26,29 @@ describe("NFT", () => {
     )) as Mesh;
   });
 
-  it("Should mint NFT for address", async () => {
+  it("Should mint NFT for meshie's address", async () => {
     expect(await nft.totalSupply()).to.equal(ethers.constants.Zero);
     await nft.mint(
-      await deployer.getAddress(),
+      meshie.address,
       0 // 0 will use block.timestamp
     );
     expect(await nft.totalSupply()).to.equal(ethers.constants.One);
+    expect(await nft.balanceOf(meshie.address)).to.equal(1);
+    expect(await nft.ownerOf(0)).to.equal(meshie.address);
     const metadata = await nft.mesh(0);
     expect(metadata.startTimestamp.gt(0)).to.be.true;
     expect(metadata.endTimestamp).to.equal(0);
     expect(metadata.probationSeconds).to.equal(30 * 3 * 24 * 60 * 60);
     expect(await nft.onProbation(0)).to.equal(true);
     expect(await nft.employed(0)).to.equal(true);
+  });
+
+  it("Should not allow meshie to transfer NFT", async () => {
+    await expect(nft.connect(meshie).transferFrom(
+      meshie.address,
+      Wallet.createRandom().address,
+      0
+    )).to.be.revertedWith("Not so fast");
   });
 
   it("Should end the probation after 90 days passed", async () => {
