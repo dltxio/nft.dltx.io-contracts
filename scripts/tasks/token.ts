e6604aac981345625319d2c9a5889db954c3447d
Lorenzo Fontoura <lorenzo.fontoura@dltx.io>
2022-07-22 16:42:50 +1000

    rewrite mint task

--- scripts/tasks/token.ts
+++ scripts/tasks/token.ts
@@ -2,44 +2,36 @@ import { task } from "hardhat/config";
 import contracts from "../../contracts.json";
 import { ethers as tsEthers } from "ethers";
 import { getLedgerSigner } from "../utils";
-
-task("read-balance")
-  .addParam("address")
-  .setAction(async (args, hre) => {
-    const contractAddress = contracts[hre.network.name].token;
-    console.log(`network is ${hre.network.name}`);
-    console.log(`token address is ${contractAddress}`);
-    const token = (await hre.ethers.getContractFactory("Token")).attach(
-      contractAddress
-    );
-    const balance = await token.balanceOf(args.address);
-    console.log(
-      `balance is ${balance.toString()} wei for address ${args.address}`
-    );
-  });
+import {Mesh} from "../../build/typechain";
 
 task("mint")
   .addParam("address")
-  .addParam("amount")
+  .addParam("startdate")
   .addOptionalParam("ledgersigner")
   .setAction(async (args, hre) => {
+    if (!hre.ethers.utils.isAddress(args.address))
+      return console.error("Invalid address");
     let signer: tsEthers.Signer;
     if (!args.ledgersigner) {
       signer = (await hre.ethers.getSigners())[0];
     } else {
       signer = getLedgerSigner(args.ledgersigner, hre.ethers.provider);
     }
-    const contractAddress = contracts[hre.network.name].token;
+    const contractAddress = contracts[hre.network.name].mesh;
     console.log(`network is ${hre.network.name}`);
-    console.log(`token address is ${contractAddress}`);
-    const token = (await hre.ethers.getContractFactory("Token"))
+    console.log(`mesh nft address is ${contractAddress}`);
+    console.log(`minting for address ${args.address}`);
+    const epoch = dateStringToStartTimestamp(args.startdate);
+    console.log(`start date is ${args.startdate} (epoch ${epoch})`);
+    const token = (await hre.ethers.getContractFactory("Mesh"))
       .attach(contractAddress)
-      .connect(signer);
-    const decimals = await token.decimals();
-    // Mint with the correct decimals.
-    const mintAmount = hre.ethers.utils.parseUnits(args.amount, decimals);
-    const receipt = await token.mint(args.address, mintAmount);
-    console.log("waiting for confirmation...");
-    await receipt.wait(1);
+      .connect(signer) as Mesh;
+    await token.mint(args.address, hre.ethers.BigNumber.from(epoch));
     console.log(`minted ${args.amount} for address ${args.address}`);
   });
+
+const dateStringToStartTimestamp = (date: string) => {
+  const split = date.split("/").map(date => Number(date));
+  const parsed = new Date(split[2], split[1] - 1, split[0]);
+  return Math.floor(parsed.getTime()/1000);
+};
